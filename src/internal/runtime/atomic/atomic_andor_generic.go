4998c2cc645ed0742f1207ff78b88616d14d2e76
Morgan Hallgren <morganh@axis.com>
2025-02-10 13:38:26 +0100

    Revert "runtime/internal/atomic: add mips operators for And/Or"
    This reverts commit 9623a3586fc6c2d11820f412d437de1a613f755c.

--- src/internal/runtime/atomic/atomic_andor_generic.go
+++ src/internal/runtime/atomic/atomic_andor_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build arm || wasm
+//go:build arm || mips || mipsle || mips64 || mips64le || wasm
 
 // Export some functions via linkname to assembly in sync/atomic.
 //
